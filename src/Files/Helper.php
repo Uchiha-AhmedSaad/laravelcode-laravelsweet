63f44a41a76ccf83ea37134de6f0b005ad493798
Ahmed Saad <ahmed.saad@sysadmn.com>
2019-02-18 21:40:59 +0200

    wrong data

--- src/Files/Helper.php
+++ src/Files/Helper.php
@@ -1,9 +1,39 @@
 <?php 
+
+use Laravelcode\LaravelSweet\Classes\SweetAlertHelper;
+
 	if (!function_exists('Sweet_Session_fired')) {
 		function Sweet_Session_fired()
 		{
 			return session()->forget(['Flash_message','title','icon','timer','text']);
 		}
+	}
+	if (!function_exists('SweetFlash')) {
+
+		function SweetFlash($element,$method,$status = "success",$time = 5000)
+		{
+			return new SweetAlertHelper($element,$method,$status,$time);
+		}
+	}
+	
+	if (!function_exists('sweetElement')) 
+	{
+		function sweetElement($param)
+		{
+			if ($param == 'Flash_message') {
+				return Session::get('Flash_message');
+			}
+			if ($param == 'title') {
+				return Session::get('title');
+			}
+			if ($param == 'timer') {
+				return Session::get('timer');
+			}
+			if ($param == 'text') {
+				return Session::get('text');
+			}
+		}
+		
 	}
 
 
