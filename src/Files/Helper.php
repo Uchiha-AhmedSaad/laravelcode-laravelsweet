9c84203fa57e63784530c2ee0579de388ffb61b0
Ahmed Saad <ahmed.saad@sysadmn.com>
2019-03-19 04:45:40 +0200

    close session

--- src/Files/Helper.php
+++ src/Files/Helper.php
@@ -5,7 +5,7 @@ use Laravelcode\LaravelSweet\Classes\SweetAlertHelper;
 	if (!function_exists('Sweet_Session_fired')) {
 		function Sweet_Session_fired()
 		{
-			return session()->forget(['Flash_message','title','icon','timer','text']);
+			return session_write_close ();
 		}
 	}
 	if (!function_exists('SweetFlash')) {
